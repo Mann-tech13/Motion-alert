63b0d221e7581b7f528f025c265112f1f8ad956e
Mann-tech13 <mannjshah01@gmail.com>
2021-06-11 14:14:44 +0530

    Update upto 11-06-2021

--- SendFile.py
+++ SendFile.py
@@ -4,64 +4,63 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 
+
 def sendEmail():
 
-  # your address information
-  fromaddr = "Sender's address"
-  toaddr = "Receiver's address" 
+    # your address information
+    fromaddr = "Sender's address"
+    toaddr = "Receiver's address"
 
-  # instance of MIMEMultipart
-  msg = MIMEMultipart()
+    # instance of MIMEMultipart
+    msg = MIMEMultipart()
 
-  # storing the senders email address  
-  msg['From'] = fromaddr
-    
-  # storing the receivers email address 
-  msg['To'] = toaddr
-    
-  # storing the subject 
-  msg['Subject'] = "Captured Pics"
-    
-  # string to store the body of the mail
-  body = ""
-    
-  # attach the body with the msg instance
-  msg.attach(MIMEText(body, 'plain'))
-    
-  # open the file to be sent 
-  filename = "output.mp4"
-  attachment = open("output.mp4", "rb")
-    
-  # instance of MIMEBase and named as p
-  p = MIMEBase('application', 'octet-stream')
-    
-  # To change the payload into encoded form
-  p.set_payload((attachment).read())
-    
-  # encode into base64
-  encoders.encode_base64(p)
-    
-  p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-    
-  # attach the instance 'p' to instance 'msg'
-  msg.attach(p)
-    
-  # creates SMTP session
-  s = smtplib.SMTP('smtp.gmail.com', 587)
-    
-  # start TLS for security
-  s.starttls()
-    
-  # Authentication
-  s.login(fromaddr, "your password")
-    
-  # Converts the Multipart msg into a string
-  text = msg.as_string()
-    
-  # sending the mail
-  s.sendmail(fromaddr, toaddr, text)
-    
-  # terminating the session
-  s.quit()
+    # storing the senders email address
+    msg['From'] = fromaddr
 
+    # storing the receivers email address
+    msg['To'] = toaddr
 
+    # storing the subject
+    msg['Subject'] = "Captured Pics"
+
+    # string to store the body of the mail
+    body = ""
+
+    # attach the body with the msg instance
+    msg.attach(MIMEText(body, 'plain'))
+
+    # open the file to be sent
+    filename = "output.mp4"
+    attachment = open("output.mp4", "rb")
+
+    # instance of MIMEBase and named as p
+    p = MIMEBase('application', 'octet-stream')
+
+    # To change the payload into encoded form
+    p.set_payload((attachment).read())
+
+    # encode into base64
+    encoders.encode_base64(p)
+
+    p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+
+    # attach the instance 'p' to instance 'msg'
+    msg.attach(p)
+
+    # creates SMTP session
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+
+    # start TLS for security
+    s.starttls()
+
+    # Authentication
+    s.login(fromaddr, "your password")
+
+    # Converts the Multipart msg into a string
+    text = msg.as_string()
+
+    # sending the mail
+    s.sendmail(fromaddr, toaddr, text)
+
+    # terminating the session
+    s.quit()
